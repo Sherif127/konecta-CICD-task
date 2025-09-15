ad0cff4b727473188e7edc31df248a24ff9f9471
Sherif Shaban <SherifShabanoo99@gmail.com>
2025-09-15 04:27:13 +0300

    first committt

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,17 @@
-output "public_ip" {
-  description = "Public IP of created instance"
+output "EC2_Public_IP" {
+  description = "Public IP of the backend EC2 instance"
   value       = aws_instance.web.public_ip
 }
 
-output "instance_id" {
-  value = aws_instance.web.id
+resource "local_file" "ansible_inventory_file_creation" {
+  content = <<-EOT
+    [all:vars]
+    ansible_user=ec2-user
+    ansible_ssh_private_key_file=~/.ssh/mykeypair.pem
+
+    [backend]
+    backend-ec2 ansible_host=${aws_instance.web.public_ip}
+  EOT
+
+  filename = "${path.module}/../Ansible/inventory.ini"
 }
